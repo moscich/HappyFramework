8ae295436e38da8bb97e61b68d9b4fdfeb4772de
Marek Mościchowski <marek.moscichowski@codewise.com>
2017-12-21 20:45:36 +0100

    Initial Commit

---
+++ HappyFramework/HappyFramework.h
@@ -0,0 +1,19 @@
+//
+//  HappyFramework.h
+//  HappyFramework
+//
+//  Created by Marek Mościchowski on 21.12.2017.
+//  Copyright © 2017 Marek Mościchowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HappyFramework.
+FOUNDATION_EXPORT double HappyFrameworkVersionNumber;
+
+//! Project version string for HappyFramework.
+FOUNDATION_EXPORT const unsigned char HappyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HappyFramework/PublicHeader.h>
+
+
