ea204280698c3eeb369749eae0a7dd4fcfce0a3d
Marek Mościchowski <marek.moscichowski@codewise.com>
2017-12-21 20:49:31 +0100

    Added one simple class

---
+++ HappyFramework/Happy.swift
@@ -0,0 +1,16 @@
+//
+//  Happy.swift
+//  HappyFramework
+//
+//  Created by Marek Mościchowski on 21.12.2017.
+//  Copyright © 2017 Marek Mościchowski. All rights reserved.
+//
+
+import Foundation
+
+public class Happy {
+    public init() {}
+    public func method() {
+        print("Happy method")
+    }
+}
